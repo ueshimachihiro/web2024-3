c0e5287f2f59bda5807d53c7a5e2f563effdac09
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-10-22 10:23:37 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -2,4 +2,4 @@ var who66 = 'world';
 function sayhello66(){ alert('Hello,' + who66 + '!'); }
 function taro66(){ who66 = '太郎'; }
 function hanako66(){ who66 = '花子'; }
-function gakita(){ alert('が来た!'); }
+function gakita(){ alert(who66 + 'が来た' + '!'); }
