4cc0ecb7bbf237fa328e723cbef104799f42400e
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-11-12 10:14:45 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,2 +1,1 @@
-function gazo688()
-{document.getElementById('who688').innerHTML='<img src="./image/ojigi_woman.png" alt="オン">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/ojigi_woman.png" alt="オン">';}
