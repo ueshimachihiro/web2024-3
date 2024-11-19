68c7f4ced1b5c31aa93f4898e74b4cd3618a5b76
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-11-19 10:12:50 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,4 @@
-function imgchange1(){document.getElementById('woman').src='./image/';}
+function imgchange1(){document.getElementById('woman').src='./image/happy_woman2.png';}
 function imgchange2(){document.getElementById('woman').src='./image/';}
 function imgchange1(){document.getElementById('logo').src='./image/masukottocharacter.jpg';}
 function imgchange2(){document.getElementById('logo').src='./image/logo.png';}
