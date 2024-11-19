0fc213efd2ef43b1d45b7aa2f91bcb8dbf27f541
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-11-19 09:28:10 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('logo').src='./image/masukottocharacter.jpg';}
-function imgchange2(){document.getElementById('logo').src='./image/logo.png';}
+function imgchange2(){document.getElementById('logo').innerHTML='<img src="./image/logo.png" alt="城西大学のマスコットキャラクター">';}
