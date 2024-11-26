08109015b1bc7687c09dab97e79582d6aebf091b
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-11-26 10:26:53 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -5,11 +5,11 @@ var str = "一文字ずつ表示します。";
 var cnt = document.timer.moji.value.length;
 //文字が全部表示されているか確認
 if ( cnt<11 ){
-document.timer.moji.value = str.substr(0,cnt+1);}
 //現在より1文字多く切り出して表示
+document.timer.moji.value = str.substr(0,cnt+1);}
 else{
-document.timer.moji.value = "";}}
 //全て表示されたら、空文字に戻す
+document.timer.moji.value = "";}}
 function startfnc(){
 setInterval("hyoji()",1000);}
 //関数hyoji()を1000ミリ秒間隔で呼び出す
