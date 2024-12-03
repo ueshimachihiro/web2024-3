a97c181a4cef7a2edd9bd12d2c0a3041d6f446a4
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-03 10:01:55 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,18 +1,2 @@
 function imgchange5(){document.getElementById('josai logo').src='./image/masukottocharacter.jpg';}
 function imgchange6(){document.getElementById('josai logo').src='./image/logo.png';}
-//function hyoji13(){
-//表示する文字
-//var str13 = "一文字ずつ表示します。";
-//テキストボックスの文字数
-//var cnt13 = document.timer13.moji13.value.length;
-//文字が全部表示されているか確認
-//if( cnt13<11 ){
-//現在より1文字多く切り出して表示
-//document.timer13.moji13.value = str13.substr(0,cnt13+1);}
-//else{
-//全て表示されたら、空文字に戻す
-//document.timer13.moji13.value ="";}
-//setTimeout()を含む関数を呼び出す
-//setTimeout("hyoji13()",1000);}
-
-//img src='./image/logo.png' id="josai logo" alt="Josai Logo";
