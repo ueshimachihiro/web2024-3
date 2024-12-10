ebf15f9503cfaa406f00abf897338dc1b0e366ae
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-10 10:31:34 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,5 +1,5 @@
 function add(){
- var x = document.getElementById('input1').value.Number;
- var y = document.getElementById('input2').value.Number;
+ var x = document.getElementById('input1').value;
+ var y = document.getElementById('input2').value;
  alert(Number('x')+Number('y'));
 }
