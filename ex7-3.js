81f4854af41f45c2407075dc83c8bffb0c0a7ab5
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-13 17:22:43 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,6 +1,7 @@
 function add(){
- return text.replace('、', '.');
  var x = Number(document.getElementById('input3').value);
  var y = Number(document.getElementById('input4').value);
+ var xFormatted = x.toString().replace('、', '.');
+ var yFormatted = y.toString().replace('、', '.');
  alert(y/(x*x));
 }
