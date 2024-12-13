210443f70b2b750fd19c291a5dd7c7f16ad73321
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-13 17:32:00 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -3,5 +3,7 @@ function add(){
  var y = Number(document.getElementById('input4').value);
  var xFormatted = x.toString().replace('、', '.');
  var yFormatted = y.toString().replace('、', '.');
+ var result = Number(xFormatted)
+ var result = Number(yFormatted)
  alert(y/(x*x));
 }
