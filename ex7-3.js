78b73b4eb88469cd0de65ac4aed034b7787d56fa
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-13 16:41:30 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,6 +1,6 @@
 function squareAndDivide(){
  var x = Number(document.getElementById('input3').value);
  var y = Number(document.getElementById('input4').value);
- var result = Math.pow(y/x*x);
+ var result = y/(x*x);
  alert(result);
 }
