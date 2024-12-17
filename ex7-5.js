b6b72a835ff89810d13778a70b9bc814f259f5b4
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-17 09:38:47 +0900

    Update ex7-5.js

--- ex7-5.js
+++ ex7-5.js
@@ -1,5 +1,6 @@
 function nazo75(){
   if(document.getElementById('answer75').value == '夢')
-  {alert('あたり!');
-  }
+  {alert('あたり!');}
+  if(document.getElementById('answer75').value == '夢')
+  else{alert('はずれ!');}
 }
