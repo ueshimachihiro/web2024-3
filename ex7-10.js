8eb179eeae2f2d25d0b1a00973277a95bc6ce9e8
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-17 13:52:43 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,9 +1,9 @@
 function bmi710(){
  var x = Number(document.getElementById('input5').value);
  var y = Number(document.getElementById('input6').value);
- if(document.getElementById('y/(x*x)').value < 1.8)
+ if('y/(x*x)'.value < 1.8)
   {alert('低体重');}
-  else if(document.getElementById('y/(x*x)').value < 25)
+  else if('y/(x*x)'.value < 25)
   {alert('普通');}
   else{alert('肥満');}
 }
