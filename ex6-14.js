763562bbbf786f8d18ef4aec614f52a0200eec0f
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2025-01-31 21:40:47 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementById('josai logo').src='./image/masukotto.jpg';}
-function imgchange6(){document.getElementById('josai logo').src='./image/logo.png';}
+function imgchange5(){document.getElementById('josai_logo').src='./image/masukotto.jpg';}
+function imgchange6(){document.getElementById('josai_logo').src='./image/logo.png';}
